069a30b459431cc08dc0196f657ded0e3fd7fee7
SCIS3-PC10\SCIS3-10 <punto2@gmail.com>
2022-10-06 19:18:07 -0500

    Primer Parcial, Almacen articulo

---
+++ src/test/java/com/parcial1191862191795/almacen/AlmacenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parcial1191862191795.almacen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlmacenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
